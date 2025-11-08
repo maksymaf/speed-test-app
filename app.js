367b1813c37cb543498ac550f5f1bad4cc69d85c
maksymaf <forbidden344@gmail.com>
2025-11-08 11:20:22 +0200

    feat(auth): Add Register and Login API

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: ["http://localhost:3000/"] }));
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+    })
+}));
 app.use(express.static('public'));
+app.use('/api/auth', require('./routes/authRoute'));
 
 (async function main() {
     try{
